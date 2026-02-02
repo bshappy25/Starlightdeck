61d61ac9779e657ee10e8e4c533f238468f1329b
bshappy25 <bendude25@aol.com>
2026-02-01 22:10:41 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,5 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
-import streamlit as st
-
 st.set_page_config(
     page_title="Starlight Deck",
     layout="centered",
@@ -33,6 +27,3 @@ with col2:
     st.button("Start Rapid")
 
 st.caption("Careon Ȼ • Community-powered • Test deployment")
-
-Add minimal Streamlit landing page
-
